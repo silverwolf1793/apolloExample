cfec7457d9daaf5bda92321de2a34975460ea5d8
psycake <52770775+psycake@users.noreply.github.com>
2023-04-21 09:29:29 -0600

    Agregar un intento de convertir todos los campos a eDgYcAsE

--- src/resolvers/personas.ts
+++ src/resolvers/personas.ts
@@ -1,4 +1,34 @@
+import { iPersona } from "types";
 import { iContext } from "../index";
-export const personas = async (parent: any, args: any, { db }: iContext, info: any) => {
-  return await db.sequelize.models.persona.findAll();
+
+const alternateCase =  (s:string)  => {
+  var chars = s.toLowerCase().split("");
+  for (var i = 0; i < chars.length; i += 2) {
+    chars[i] = chars[i].toUpperCase();  
+  }
+  return chars.join("");
 };
+
+export const personas = async (parent: any, args: any, { db }: iContext, info: any) => {
+  const personas: iPersona[] = await db.sequelize.models.persona.findAll() as any;
+
+  const personaEdgy = personas.map((persona:iPersona)=>{
+    persona.apellido = alternateCase(persona.apellido||"");
+    persona.nombre = alternateCase(persona.nombre||"");
+    persona.email = alternateCase(persona.email||"");
+    persona.telefono = persona.telefono;
+    persona.id = persona.id;
+  })
+
+  // let nuevoArray:any=[]
+  // for(let i=0;i<=personas.length; i++){
+  //    alternateCase(personas[i])
+  // }
+  // return nuevoArray
+
+  return personaEdgy;
+};
+
+// export const personas = async (parent: any, args: any, { db }: iContext, info: any) => {
+//  return await db.sequelize.models.persona.findAll();
+// };
